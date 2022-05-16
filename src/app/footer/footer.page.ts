f4443e8ed32544411336a2a781a0288169c97f3d
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2022-05-16 11:22:57 +0100

    feat: composición de la barra de navegación

--- src/app/footer/footer.page.ts
+++ src/app/footer/footer.page.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
+
+import {
+  boardIconFilled,
+  boardIconOutline,
+  boardTitle,
+  libraryIconFilled,
+  libraryIconOutline,
+  libraryTitle,
+  listsIconFilled,
+  listsIconOutline,
+  listsTitle
+} from 'src/assets/constants/my-game-library.contants';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +20,35 @@ import { Component } from '@angular/core';
 })
 export class FooterPage {
 
-  constructor() {}
+  @ViewChild('footer') footer: IonTabs;
+
+  public board: string;
+  public library: string;
+  public lists: string;
+
+  public updateFooterIcons(tabSelected: any): void {
+    switch (tabSelected) {
+      case boardTitle:
+        this.board = boardIconFilled;
+        this.library = libraryIconOutline;
+        this.lists = listsIconOutline;
+        break;
+      case libraryTitle:
+        this.board = boardIconOutline;
+        this.library = libraryIconFilled;
+        this.lists = listsIconOutline;
+        break;
+      case listsTitle:
+        this.board = boardIconOutline;
+        this.library = libraryIconOutline;
+        this.lists = listsIconFilled;
+        break;
+      default:
+        this.board = boardIconOutline;
+        this.library = libraryIconOutline;
+        this.lists = listsIconOutline;
+        break;
+    }
+  }
 
 }
---
+++ src/assets/constants/my-game-library.contants.ts
@@ -0,0 +1,11 @@
+export const boardTitle = 'board';
+export const boardIconFilled = 'clipboard';
+export const boardIconOutline = 'clipboard-outline';
+
+export const libraryTitle = 'library';
+export const libraryIconFilled = 'library';
+export const libraryIconOutline = 'library-outline';
+
+export const listsTitle = 'lists';
+export const listsIconFilled = 'list';
+export const listsIconOutline = 'list-outline';
